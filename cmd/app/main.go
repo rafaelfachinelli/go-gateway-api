d819ba499f3bee21d22704ec6c497ef9ba1e6a18
rafaelfachinelli <rafael.l.a.fachinelli@gmail.com>
2025-04-09 23:00:06 -0300

    feat: Load environment variables and establish database connection in main application

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/devfullcycle/imersao22/go-gateway/internal/repository"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/web/server"
+	"github.com/joho/godotenv"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
 func main() {
-	println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Databae connection string
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error opening database")
+	}
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	server := server.NewServer(accountService, getEnv("HTTP_PORT", "8080"))
+	server.ConfigureRoutes()
+
+	if err := server.Start(); err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
