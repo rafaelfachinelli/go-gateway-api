870f0f1286aefa341d77142c933006ad45f7e99d
rafaelfachinelli <rafael.l.a.fachinelli@gmail.com>
2025-04-09 22:21:33 -0300

    feat: Enhance account management with error handling, repository updates, and new HTTP handlers

--- internal/dto/account.go
+++ internal/dto/account.go
@@ -1,6 +1,10 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+)
 
 type CreateAccountInput struct {
 	Name  string `json:"name"`
@@ -15,4 +19,20 @@ type AccountOutput struct {
 	APIKey    string    `json:"api_key"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func ToAccount(input CreateAccountInput) *domain.Account {
+	return domain.NewAccount(input.Name, input.Email)
+}
+
+func FromAccount(account *domain.Account) AccountOutput {
+	return AccountOutput{
+		ID:        account.ID,
+		Name:      account.Name,
+		Email:     account.Email,
+		Balance:   account.Balance,
+		APIKey:    account.APIKey,
+		CreatedAt: account.CreatedAt,
+		UpdatedAt: account.UpdatedAt,
+	}
 }
--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AccountRepository
@@ -8,4 +11,60 @@ type AccountService struct {
 
 func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIKey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = s.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) FindByAPIKey(apiKey string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) FindByID(id string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
 }
